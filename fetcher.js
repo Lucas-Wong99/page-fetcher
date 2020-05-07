d87681ce0bc212dfee24c2564cdd5a830bef08e1
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-07 04:06:37 +0000

    Full functionality

--- fetcher.js
+++ fetcher.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 
-request(args[0], (error, data) => {
+request(args[0], (error, response, data) => {
   fs.writeFile(args[1], data, () => {
+    if (response.statusCode !== 200) {
+      throw err;
+    }
     if (error) {
       throw err;
     }
-    console.log(`Downloaded and saved ${data.headers['content-length']} bytes to ${args[1]}`
-    );
+    console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${args[1]}`);
   });
 });
 
