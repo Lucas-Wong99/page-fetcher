e7e936198e78472f1a36c89e75ac4a8235da8075
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-07 04:23:24 +0000

    Refactored

--- fetcher.js
+++ fetcher.js
@@ -5,11 +5,16 @@ const args = process.argv.slice(2);
 
 request(args[0], (error, response, data) => {
   fs.writeFile(args[1], data, () => {
+    fs.stat(args[1], (error, stats) => {
+      if (error) {
+        console.log("Hi guys, sorry you've chosen the incorrect file path");
+      }
+    });
     if (response.statusCode !== 200) {
-      throw err;
+      throw error;
     }
     if (error) {
-      throw err;
+      throw error;
     }
     console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${args[1]}`);
   });
